cb83882cb963bd77def9e1a676b455e513fcf1db
DIEGO-LIMA <164493983+celsodiego@users.noreply.github.com>
2024-10-16 16:22:19 +0100

    Add files via upload

---
+++ BDPORTUGAL2.sql
@@ -0,0 +1,25 @@
+INSERT INTO Cidade (
+	código_postal,nome_da_cidade
+    )
+    VALUES (
+	1000  , "Lisboa"),
+    (4150 , "Porto"),
+    (4430 , "Vila Nova de Gaia"),
+    (1500 , "Amadora"),
+    (4700 , "Braga"),
+    (9000 , "Funchal"),
+    (3030 , "Coimbra"),
+    (2900, "Setúbal"),
+    (2735 , "Alguava-Cacém"),
+    (2605 , "Queluz"),
+    (4800 , "Guimarães"),
+    (4436 , "Rio Tinto"),
+    (2830 , "Barreiro"),
+    (2400 , "Leiria"),
+    (1675 , "Odivelas"),
+    (3500 , "Viseu"),
+    (3800 , "Aveiro"
+    );
+    
+SELECT * FROM Cidade;
+    
